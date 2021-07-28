815339c8c77cf4ec8cb1ed507713ff8d696aa3cf
Slobodan Zivkovic <slobacartoonac@gmail.com>
2021-07-28 16:05:39 +0200

    Add fancy grid

--- phy_edit/src/javascript/drawGrid.js
+++ phy_edit/src/javascript/drawGrid.js
@@ -14,7 +14,7 @@ GridPloter.prototype.draw=function(sizex,sizey, view)
     var gridScale = scale
     while(gridScale> 2)
         gridScale /= 2
-    while(gridScale< 0.5)
+    while(gridScale< 1)
         gridScale *= 2
 	const stepX = sizex * gridScale
 	const stepY = sizey * gridScale
@@ -31,7 +31,24 @@ GridPloter.prototype.draw=function(sizex,sizey, view)
 		context.lineTo( canvasWidth , y)
 	}
 	context.lineWidth = 2
-	context.strokeStyle = 'gray'
+	context.strokeStyle = 'rgba(255, 255, 255, 0.5)'
+	context.stroke()
+
+	var startx=(startx + stepX/2) % stepX
+    var starty=(starty + stepY/2) % stepY
+
+	context.beginPath()
+	for (var x = startx; x <= canvasWidth; x += stepX) {
+		context.moveTo( x , 0)
+		context.lineTo( x, canvasHeight)
+	}
+
+	for (var y = starty; y <= canvasHeight; y += stepY) {
+		context.moveTo( 0, y )
+		context.lineTo( canvasWidth , y)
+	}
+	context.lineWidth = 2
+	context.strokeStyle = 'rgba(128, 128, 128, '+(gridScale-1.0)+')'
 	context.stroke()
 	context.font = '10px Arial'
 	context.fillText((Math.round(view.x * 100) / 100 + Number.EPSILON)+','+(Math.round(view.y * 100 + Number.EPSILON) / 100)+','+(Math.round(view.scale * 100 + Number.EPSILON) / 100),10,50)
